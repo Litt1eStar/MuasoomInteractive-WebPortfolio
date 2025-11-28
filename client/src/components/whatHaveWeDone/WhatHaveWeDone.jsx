95bff6030996d7ed80d73fce315aff8cf295d0ca
Woody <devkritt@gmail.com>
2025-11-29 03:17:59 +0700

    create mockup page for hero what have we done sections

--- client/src/components/whatHaveWeDone/WhatHaveWeDone.jsx
+++ client/src/components/whatHaveWeDone/WhatHaveWeDone.jsx
@@ -5,7 +5,13 @@ const WhatHaveWeDone = () => {
   return (
     <>
         <div className={styles.container}>
-
+            <div className={styles.titleContainer}>
+                <div className={styles.title}>"WHAT WE HAVE DONE"</div>
+                <div className={styles.seeAllBtn}>See All Projects</div>
+            </div>
+            <div className={styles.workContainer}>
+                
+            </div>
         </div>
     </>
   )
