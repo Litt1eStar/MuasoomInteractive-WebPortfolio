88d92bbe8622063ccc36e249ab1a97586ccc7d2e
Woody <devkritt@gmail.com>
2025-11-29 02:40:14 +0700

    create mockup page for hero our services section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 import TopWork from "./components/topWork/TopWork"
+import OurServices from "./components/ourServices/OurServices"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <TopWork />
+      <OurServices />
     </>
   )
 }
