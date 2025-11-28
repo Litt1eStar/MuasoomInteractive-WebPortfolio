40172653754788c99f508e47886b315c4e436825
Woody <devkritt@gmail.com>
2025-11-29 02:50:10 +0700

    edit design for another device of our services section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/hero/Hero"
 import Navbar from "./components/navbar/Navbar"
 import TopWork from "./components/topWork/TopWork"
 import OurServices from "./components/ourServices/OurServices"
+import WhatHaveWeDone from "./components/whatHaveWeDone/WhatHaveWeDone"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <TopWork />
       <OurServices />
+      <WhatHaveWeDone />
     </>
   )
 }
---
+++ client/src/components/whatHaveWeDone/WhatHaveWeDone.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./WhatHaveWeDone.module.css"
+
+const WhatHaveWeDone = () => {
+  return (
+    <>
+        <div className={styles.container}>
+
+        </div>
+    </>
+  )
+};
+
+export default WhatHaveWeDone;
