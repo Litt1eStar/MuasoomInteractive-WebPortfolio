1212934aa071a474434d5bd598d90e9d183c5f91
Woody <devkritt@gmail.com>
2025-12-01 08:07:41 +0700

    implement real design of CTA and add animation to Button

--- client/src/components/CTA/CTA.jsx
+++ client/src/components/CTA/CTA.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import styles from "./CTA.module.css"
 
 const CTA = () => {
+  const [isButtonHovered, setIsButtonHovered] = useState(false)
+
   return (
     <>
         <div className={styles.container}>
-            <div className={styles.title}>READY TO GET START?</div>
-            <div className={styles.contactUsBtn}>Contact Us</div>
+            <div className={styles.title}>
+              <span>READY TO GET START</span>
+              <span className={styles.confuse}>?</span>  
+            </div>
+            <div 
+              className={styles.contactUsBtn} 
+              >
+                Contact Us
+              </div>
         </div>
     </>
   )
