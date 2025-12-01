29331497e31d82d7c3120d9c8fc63741606e5491
Woody <devkritt@gmail.com>
2025-12-01 15:08:56 +0700

    implement works section

--- client/src/data/worksData.js
+++ client/src/data/worksData.js
@@ -1,7 +1,14 @@
 import IMG_DOLOREM from "../img/Project/Dolorem.jpg";
-
+import IMG_PDPA from "../img/Project/PDPA.png";
+import IMG_FACTORY from "../img/Project/Factory.png";
+import IMG_WORKSHOP2 from "../img/Project/Workshop2.png";
+import IMG_ALPHALUMA from "../img/Project/AlphaLumaMatte.png";
 const WORKS_DATA = [
-    {id: 1, name: "Dolorem", types: ["Videography", "Editing", "Colour grading"], imageUrl: IMG_DOLOREM}
+    {id: 1, name: "Dolorem", types: ["Videography", "Editing", "Colour grading"], imageUrl: IMG_DOLOREM},
+    {id: 2, name: "PDPA", types: ["2D Animation", "Editing"], detail: "This project is a hand-drawn, frame-by-frame animation created for the Electronic Law class. The core purpose is to simplify and clearly explain the key principles of the Personal Data Protection Act (PDPA) through an accessible, highly engaging visual narrative.",imageUrl: IMG_PDPA},
+    {id: 3, name: "Refiber", types: ["2D Animation", "Editing"], detail: "This project is a 2D motion graphic designed to provide a comprehensive, step-by-step visual tour of a PET plastic bottle recycling plant, illustrating the transformation process from discarded bottles into usable synthetic fibers.",imageUrl: IMG_FACTORY},
+    {id: 4, name: "Vlog", types: ["Videography", "Editing"], detail: "This project is a high-energy, $\mathbf{1}$-minute video montage designed to capture the essence and key moments of a single day. The primary focus is on rhythmic and dynamic video editing to convey pace and atmosphere within a very short timeframe.",imageUrl: IMG_WORKSHOP2},
+    {id: 5, name: "Alpha&LumaMatte", types: ["3D Animation", "Editing"], detail: "This project is a 3D motion graphic animation designed to showcase the JOLI sunscreen product, featuring a unique branding identity inspired by P To. The animation focuses on highlighting both the product's aesthetic design and its key benefits.",imageUrl: IMG_ALPHALUMA}
 ]
 
 export default WORKS_DATA;
--- client/src/pages/WorkDetail/WorkDetail.jsx
+++ client/src/pages/WorkDetail/WorkDetail.jsx
@@ -1,7 +1,5 @@
 import WORKS_DATA from "../../data/worksData";
 import styles from "./WorkDetail.module.css";
-
-import IMG_DOLOREM from "../../img/Project/Dolorem.jpg";
 import { WorkType } from "../../components/WorkCard/WorkCard";
 import CTA from "../../components/CTA/CTA";
 
@@ -13,8 +11,7 @@ const WorkDetail = () => {
   const targetId = parseInt(workIdString, 10);
   const project = WORKS_DATA.find(p => p.id == targetId);
 
-  const workImagePath = {backgroundImage: `url(${IMG_DOLOREM})`}
-  console.log(project)
+  const workImagePath = {backgroundImage: `url(${project.imageUrl})`}
 
   return (
     <>
@@ -23,7 +20,7 @@ const WorkDetail = () => {
             <div className={styles.workImg} style={workImagePath}></div>
             <div className={styles.detailContainer}>
                 <div className={styles.subHeader}>DETAIL</div>
-                <div className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit labore doloremque praesentium, ea nobis est, debitis sequi voluptatibus ratione eligendi quaerat dignissimos quibusdam optio adipisci! Enim blanditiis est quisquam libero culpa saepe, distinctio tempore aliquam accusantium quasi quod voluptatibus fuga id velit quaerat temporibus ipsa iusto dicta? Cumque, molestiae nam.</div>
+                <div className={styles.description}>{project.detail}</div>
                 <div className={styles.subHeader}>Duration : 1 week</div>
                 <div className={styles.typeContainer}>
                     {project.types.map((type) => (
